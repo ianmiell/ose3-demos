1e746fea4712cb7d6f55cb1a6efcbd1481ede56d
Ian Miell <ian.miell@gmail.com>
2015-03-31 10:49:09 +0100

    ui

--- bin/list.sh
+++ bin/list.sh
@@ -2,4 +2,9 @@
 
 # nb: doesn't get projects
 
-osc get builds,buildconfigs,deploymentconfigs,imagerepositories,images,pods,replicationcontrollers,routes,services
+for resource_type in builds buildconfigs deploymentconfigs imagerepositories images pods replicationcontrollers routes services
+do
+	echo "# Entity: ${resource_type}"
+	osc get ${resource_type}
+	osc get ${resource_type} | grep -v "^[A-Z ]*$" | awk '{print $1}' | sed "s/\(.*\)/osc delete ${resource_type} \1"
+done
